c34c3e7558b679f97070e51c6159bbc76bf6a0b2
Dmitrij Zherebjatjew <dzherebjatjew@thumbtack.net>
2013-03-11 11:55:06 +0700

    Create project wireframe

---
+++ src/com/android/dj/morse/MorseReader.java
@@ -0,0 +1,15 @@
+package com.android.dj.morse;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MorseReader extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
